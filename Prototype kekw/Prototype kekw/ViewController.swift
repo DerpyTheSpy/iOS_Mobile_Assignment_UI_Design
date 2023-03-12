edab86edeaa7c6914b9185d54881e8195d7db3dd
DerpyTheSpy <erickim334@gmail.com>
2023-03-12 11:46:32 -0400

    first commit

---
+++ Prototype kekw/Prototype kekw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototype kekw
+//
+//  Created by eric kim on 2023-02-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
